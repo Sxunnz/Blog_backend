b8086ce7475d62a6ecf9df5eacd478f20f2f035f
Sxunnz <1577055352@qq.com>
2022-10-17 16:57:39 +0800

    update

--- sanxun-blog/src/main/java/com/sanxun/SanXunBlogApplication.java
+++ sanxun-blog/src/main/java/com/sanxun/SanXunBlogApplication.java
@@ -1,4 +1,16 @@
 package com.sanxun;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+/**
+ * 启动类
+ */
 public class SanXunBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanXunBlogApplication.class,args);
+    }
 }
