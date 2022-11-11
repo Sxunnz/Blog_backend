5ccb4ebb8a19b0fdee085818abf751922cf23500
Sxunnz <1577055352@qq.com>
2022-11-11 15:53:44 +0800

    Update ArticleServiceImpl.java

--- sanxun-framework/src/main/java/com/sanxun/service/impl/ArticleServiceImpl.java
+++ sanxun-framework/src/main/java/com/sanxun/service/impl/ArticleServiceImpl.java
@@ -2,6 +2,7 @@ package com.sanxun.service.impl;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.sanxun.domain.ResponseResult;
 import com.sanxun.domain.entity.Article;
@@ -21,10 +22,10 @@ public class ArticleServiceImpl extends ServiceImpl<ArticleMapper, Article> impl
         //按照浏览量进行排序
         queryWrapper.orderByDesc(Article::getViewCount);
         //最多只查询10条
-//        Page<Article> page = new Page(1,10);
-//        page(page,queryWrapper);
-//
-//        List<Article> articles = page.getRecords();
-//        return ResponseResult.okResult(articles);
+        Page<Article> page = new Page(1,10);
+        page(page,queryWrapper);
+
+        List<Article> articles = page.getRecords();
+        return ResponseResult.okResult(articles);
     }
 }
